6c4ad9d2d282a80de910c8b61d311d00cd27b5d5
Slee Woo <mail@sleewoo.com>
2024-02-18 18:41:59 +0200

    refined bodyparser

--- lib/bodyparser/config.ts
+++ lib/bodyparser/config.ts
@@ -1,22 +1,20 @@
-
 import type { JsonOptions, FormOptions, RawOptions } from "./@types";
 
 export const json: JsonOptions = {
   limit: 1024 ** 2,
-  trim: [ "*" ],
-}
+  trim: ["*"],
+};
 
 export const form: FormOptions = {
   limit: 1024 ** 2,
-}
+};
 
 export const raw: RawOptions = {
   limit: 1024 ** 2,
-}
+};
 
 export default {
   json,
   form,
   raw,
-}
-
+};
--- lib/bodyparser/index.ts
+++ lib/bodyparser/index.ts
@@ -1,163 +1,128 @@
-
 import zlib from "zlib";
 
 import IncomingForm from "formidable";
 import rawParser from "raw-body";
 
+import type { Middleware } from "@appril/core/router";
+
 import type {
-  JsonOptions, FormOptions,
-  TrimOption, Trimmer, RawOptions,
+  JsonOptions,
+  FormOptions,
+  TrimOption,
+  Trimmer,
+  RawOptions,
 } from "./@types";
 
 import config from "./config";
 
-export default { config, json, form, raw }
+export default { config, json, form, raw };
 
-export { config }
-export const bodyparser = { json, form, raw }
+export { config };
+export const bodyparser = { json, form, raw };
 export * from "./@types";
 
-// importing Middleware from ../router or from koa causing type errors
-type Middleware = (ctx: any, next: Function) => Promise<void>;
+export function json(opts: JsonOptions = {}): Middleware[] {
+  return [
+    async (ctx, next) => {
+      const form = IncomingForm({
+        maxFieldsSize: opts.limit || config.json.limit,
+        ...opts,
+      });
 
-export function json(
-  opts: JsonOptions = {},
-): Middleware {
+      const trimmer = trimmerFactory(opts.trim);
 
-  return async function jsonBodyparser(ctx, next) {
+      ctx.request.body = await new Promise((resolve, reject) => {
+        form.parse(ctx.request.req, (err, fields) => {
+          if (err) {
+            return reject(err);
+          }
 
-    const form = IncomingForm({
-      maxFieldsSize: opts.limit || config.json.limit,
-      ...opts,
-    })
+          resolve(trimmer ? trimmer(fields) : fields);
+        });
+      });
 
-    const trimmer = Trimmer(opts.trim)
-
-    ctx.request.body = await new Promise((resolve, reject) => {
-
-      form.parse(ctx.request.req, (err, fields) => {
-
-        if (err) {
-          return reject(err)
-        }
-
-        resolve(
-          trimmer
-            ? trimmer(fields)
-            : fields
-        )
-
-      })
-
-    })
-
-    return next()
-
-  }
-
+      return next();
+    },
+  ];
 }
 
-export function form(
-  opts: FormOptions = {},
-): Middleware {
+export function form(opts: FormOptions = {}): Middleware[] {
+  return [
+    async (ctx, next) => {
+      const form = IncomingForm({
+        maxFieldsSize: opts.limit || config.form.limit,
+        maxFileSize: opts.limit || config.form.limit,
+        ...opts,
+      });
 
-  return async function formBodyparser(
-    ctx,
-    next,
-  ) {
+      let trimmer = trimmerFactory(opts.trim);
 
-    const form = IncomingForm({
-      maxFieldsSize: opts.limit || config.form.limit,
-      maxFileSize: opts.limit || config.form.limit,
-      ...opts,
-    })
+      if (opts.multipart || opts.urlencoded) {
+        trimmer = undefined;
+      }
 
-    let trimmer = Trimmer(opts.trim)
+      ctx.request.body = await new Promise((resolve, reject) => {
+        form.parse(ctx.request.req, (err, fields, files) => {
+          if (err) {
+            return reject(err);
+          }
 
-    if (opts.multipart || opts.urlencoded) {
-      trimmer = undefined
-    }
+          resolve({
+            fields: trimmer ? trimmer(fields) : fields,
+            files,
+          });
+        });
+      });
 
-    ctx.request.body = await new Promise((resolve, reject) => {
-
-      form.parse(ctx.request.req, (err, fields, files) => {
-
-        if (err) {
-          return reject(err)
-        }
-
-        resolve({
-          fields: trimmer
-            ? trimmer(fields)
-            : fields,
-          files,
-        })
-
-      })
-
-    })
-
-    return next()
-
-  }
-
+      return next();
+    },
+  ];
 }
 
-export function raw(
-  opts: RawOptions = {},
-): Middleware {
+export function raw(opts: RawOptions = {}): Middleware[] {
+  return [
+    async (ctx, next) => {
+      const { chunkSize, ...rawParserOptions } = { ...config.raw, ...opts };
 
-  return async function rawBodyparser(
-    ctx,
-    next,
-  ) {
+      const stream = ctx.request.req.pipe(zlib.createUnzip({ chunkSize }));
+      ctx.request.body = await rawParser(stream, rawParserOptions);
 
-    const {
-      chunkSize,
-      ...rawParserOptions
-    } = { ...config.raw, ...opts }
-
-    const stream = ctx.request.req.pipe(zlib.createUnzip({ chunkSize }))
-    ctx.request.body = await rawParser(stream, rawParserOptions)
-
-    return next()
-
-  }
-
+      return next();
+    },
+  ];
 }
 
-function Trimmer(
+function trimmerFactory(
   trimOption: TrimOption | undefined,
 ): Trimmer | undefined {
-
   if (!Array.isArray(trimOption) || !trimOption.length) {
-    return
+    return;
   }
 
   const trimableKeys: {
-    [key: string]: boolean
-  } = trimOption.reduce((m, k) => ({ ...m, [k]: true }), {})
+    [key: string]: boolean;
+  } = trimOption.reduce((m: Record<string, boolean>, k) => {
+    m[k] = true;
+    return m;
+  }, {});
 
-  const trim = (key: string, val: any) => {
-
+  const trim = (key: string, val: unknown) => {
     if (typeof val !== "string") {
-      return val
+      return val;
     }
 
-    return trimableKeys[key] || trimableKeys["*"]
-      ? val.trim()
-      : val
+    return trimableKeys[key] || trimableKeys["*"] ? val.trim() : val;
+  };
 
-  }
-
-  const reducer = (memo: Record<string, any>, [ key, val ]: [ string, any ]) => {
-    memo[key] = trim(key, val)
-    return memo
-  }
+  const reducer = (
+    memo: Record<string, unknown>,
+    [key, val]: [string, unknown],
+  ) => {
+    memo[key] = trim(key, val);
+    return memo;
+  };
 
   // accumulator is set to payload intentionally, to avoid duplication of big strings!
   // if using a new object for accumulator then trimmed strings will be duplicated!
-  return (payload) => Object.entries(payload).reduce(reducer, payload)
-
+  return (payload) => Object.entries(payload).reduce(reducer, payload);
 }
-
